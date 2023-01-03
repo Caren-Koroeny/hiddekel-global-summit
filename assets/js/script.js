7512a84b911bd6476ca50edbc5cf351d74825936
Caren-Koroeny <carensiya5@gmail.com>
2023-01-03 09:56:19 +0300

    added footer section

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,3 +82,26 @@ speakerCardSection.forEach((ele, index) => {
 	card.appendChild(cardContainer);
 
 });
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
