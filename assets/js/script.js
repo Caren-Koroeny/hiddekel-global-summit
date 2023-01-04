c87b1d771c3a3b957154a6234e2c8682f30f6adf
Caren-Koroeny <carensiya5@gmail.com>
2023-01-04 17:16:16 +0300

    deleted the jquery codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,29 +92,5 @@ const seeMoreBtn =  `
 		`;
 	speakerSection.innerHTML += seeMoreBtn;
 
-$(document).ready(function () {
-	$(".customer-logos").slick({
-	  slidesToShow: 6,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 1500,
-	  arrows: false,
-	  dots: false,
-	  pauseOnHover: false,
-	  responsive: [
-		{
-		  breakpoint: 768,
-		  settings: {
-			slidesToShow: 4
-		  }
-		},
-		{
-		  breakpoint: 520,
-		  settings: {
-			slidesToShow: 3
-		  }
-		}
-	  ]
-	});
-  });
+
   
